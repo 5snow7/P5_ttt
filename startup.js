8a7100f018e8e7c6bde675f70f00cfa15b9a7a89
PhsTigers <mwomack@pcoe.k12.ca.us>
2020-04-18 10:53:41 -0700

    Update startup.js

--- startup.js
+++ startup.js
@@ -5,7 +5,7 @@ par=createP('Click screen to play again.');
 par.position(100,15);par.class("parbdd");
 par2=createP('Choose a Surface to play on');
 par2.position(450,15);par2.class("parbdd");
-
+//comment
 rad=createRadio('Choose a surface to play on');
 rad.option('flat',1);
 rad.option('cylinder',2);
